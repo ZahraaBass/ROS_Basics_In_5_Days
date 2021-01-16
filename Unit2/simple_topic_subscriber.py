167833a56820ebeb437e2992d2d6e12b7ff66e0c
Zahraa Bassyouni <76676384+ZahraaBass@users.noreply.github.com>
2021-01-16 10:04:37 +0200

    Create simple_topic_subscriber.py

---
+++ Unit2/simple_topic_subscriber.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+import rospy 
+from nav_msgs.msg import Odometry
+
+# Create a function that will print the data in "msg"
+def listener(msg):
+	# To print the Whole msg on the topic:
+	# print msg
+	# To print specific parts of the msg use:
+	# print msg.header
+	  print msg.pose
+	# print msg.orientation
+
+# Create a node called "topic_subscriber"       
+rospy.init_node("topic_subscriber")
+
+# Subscribe to the topic: /husky_velocity_controller/odom , which has messages of type: Odometry flowing into it, and call the function listener, which prints out these msgs. 
+sub = rospy.Subscriber('/husky_velocity_controller/odom', Odometry, listener)
+
+# Keep doing this till infinity:
+rospy.spin()
+
+# rospy. spin() will effectively go into an infinite loop until it receives a shutdown signal (e.g. ctrl-c ). During that loop it will process any events that occur, such as data received on a topic or a timer triggering, when they occur but otherwise it will sleep..
